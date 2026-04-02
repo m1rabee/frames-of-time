f0972541195369d00997f640a4d648e91d2ec7a9
m1rabee <152761613+m1rabee@users.noreply.github.com>
2026-04-02 16:05:33 +0800

    Add frame switching functionality to animation

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const image = document.getElementById("animationFrame");
+const slider = document.getElementById("frameSlider");
+const button = document.getElementById("nextBtn");
 
+let currentFrame = 1;
+
+button.addEventListener("click", function () {
+  if (currentFrame === 1) {
+    currentFrame = 2;
+  } else {
+    currentFrame = 1;
+  }
+
+  image.src = 'assets/test-frames/frame_0${currentFrame}.png';
+
+}); 
+      
