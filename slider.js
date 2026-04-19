39b6ad495f592b1bf2b654ce00c7823cd72aaf59
m1rabee <152761613+m1rabee@users.noreply.github.com>
2026-04-19 19:28:17 +0800

    create slider with modifiable behaviors

---
+++ slider.js
@@ -0,0 +1,110 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  slider = new Slider()
+  slider.direction = "forward";
+  slider.snapTo = slider.sliderMin;
+  slider.weight = 0.25;
+  slider.clickableTrack = false;
+}
+
+function draw() {
+  background(255);
+  slider.render()
+  slider.update()
+}
+
+function mousePressed(){
+  slider.mousePressed()
+}
+
+function mouseReleased(){
+  slider.mouseReleased()
+}
+
+class Slider {
+  constructor(
+    value = 0,
+    sliderX = 100,
+    sliderY = 800,
+    isDragging = false,
+    weight = 0.05,
+    sliderMin = 100, 
+    sliderMax = 800,
+    thumbSize = 20,
+    direction = null,
+    snapTo = null,
+    clickableTrack = false
+){
+      this.sliderX = sliderX
+      this.sliderY = sliderY
+      this.isDragging = isDragging
+      this.weight = weight
+      this.sliderMin = sliderMin
+      this.sliderMax = sliderMax
+      this.thumbSize = thumbSize
+      this.direction = direction
+      this.snapTo = snapTo
+      this.clickableTrack = clickableTrack
+    }
+
+  mousePressed(){
+    // check if mouse is over thumb
+    let onThumb = dist(mouseX, mouseY, this.sliderX, this.sliderY) < this.thumbSize; 
+    
+    if (onThumb){ // allow dragging if on thumb
+      this.isDragging = true;
+    }
+
+    // check if mouse is over track 
+    let onTrack = abs(mouseY - this.sliderY) < this.thumbSize; 
+
+    if (this.clickableTrack && onTrack){ // controls whether track can be clicked or not
+        this.sliderX = constrain (mouseX, this.sliderMin, this.sliderMax); 
+        this.isDragging = true;
+    }
+}
+  
+  mouseReleased(){
+    if (this.isDragging){
+      this.isDragging = false;
+
+      // snap to sliderMin or sliderMax 
+      if (this.snapTo !== null){
+        this.sliderX = this.snapTo;
+      }
+  }
+  }
+  
+  update(){
+    if (this.isDragging){
+      let targetX = mouseX;
+      
+      // controls slider' direction (forward only, reverse only, or free)
+      if (this.direction === "forward"){
+        targetX = max(this.sliderX, mouseX);
+      } else if (this.direction === "reverse") {
+        targetX = min(this.sliderX, mouseX); 
+      } 
+
+      // constrain to slider track 
+      targetX = constrain(targetX, this.sliderMin, this.sliderMax);
+      
+      // controls weight/resistance of the thumb 
+      let speed = targetX - this.sliderX;
+      this.sliderX += speed * this.weight * (deltaTime / 16.67);
+     // this.sliderX = lerp(this.sliderX, targetX, this.weight);
+      }
+  
+  }
+  
+  render(){
+    // draw slider track
+    stroke(0)
+    strokeWeight(4)
+    line(this.sliderMin, this.sliderY, this.sliderMax, this.sliderY)
+    
+    // draw thumb
+    fill(255)
+    ellipse(this.sliderX, this.sliderY, this.thumbSize *2)
+  }
+}
